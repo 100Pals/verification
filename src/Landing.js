f604171af609e11ccd42837d5270aab9d6ff9443
SnowyLuma <lilly@snowyluma.dev>
2020-05-24 18:21:01 +0200

    Remove typos from hiki

--- src/Landing.js
+++ src/Landing.js
@@ -22,7 +22,7 @@ export default function Landing() {
       <img className="icon" src={SiteIcon} alt="AchievementHunting.com Icon" />
       <div className="divider" />
       <p>Sign in with Discord in order to start verifying ownership of your gaming accounts.</p>
-      <p>You will be prompted to verify the platforms you play game on..</p>
+      <p>You will be prompted to verify the platforms you play games on.</p>
       <div className="login">
         <a href={oauthURL}>Sign in with Discord</a>
       </div>
