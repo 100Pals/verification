6d6a783a1216436620f099113d79b91b0fd13f67
SnowyLuma <lilly@snowyluma.dev>
2020-06-02 13:28:04 +0200

    Use 256px images for logos instead of 128px

--- src/Accounts/Avatar.js
+++ src/Accounts/Avatar.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://cdn.discordapp.com";
 export default class Avatar extends Component {
   defaultImage() {
     const avatar = this.props.user.discriminator % 5;
-    const path = `/embed/avatars/${avatar}.png?size=128`;
+    const path = `/embed/avatars/${avatar}.png?size=256`;
 
     return <img className="icon" src={BASE_URL + path} alt="avatar" />;
   }
@@ -16,8 +16,8 @@ export default class Avatar extends Component {
 
     return (
       <>
-        <source srcSet={BASE_URL + path + ".webp?size=128"} type="image/webp" />
-        <img className="icon" src={BASE_URL + path + ".png?size=128"} alt="avatar" />
+        <source srcSet={BASE_URL + path + ".webp?size=256"} type="image/webp" />
+        <img className="icon" src={BASE_URL + path + ".png?size=256"} alt="avatar" />
       </>
     );
   }
