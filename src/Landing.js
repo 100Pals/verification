f1139788190302911b303e5ad8d218f1a0b92054
SnowyLuma <lilly@snowyluma.dev>
2020-06-03 16:39:13 +0200

    Add Discord branding to sign in button
    This was recommended to me to make the sign in button
    be more inline with the platform boxes on the signed-in page

--- src/Landing.js
+++ src/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { DISCORD_CLIENT_ID, IS_LOCAL, LOCATION } from "./constants";
 
+import DiscordLogo from "./media/discord-logo.svg";
 import SiteIcon from "./media/achievementhunting.com-logo.png";
 
 import "./Landing.css";
@@ -22,8 +23,11 @@ export default function Landing() {
       <img className="icon" src={SiteIcon} alt="AchievementHunting.com Icon" />
       <p>Sign in with Discord in order to start verifying ownership of your gaming accounts.</p>
       <p>You will be prompted to verify the platforms you play games on.</p>
-      <div className="login">
-        <a href={oauthURL}>Sign in with Discord</a>
+      <div className="wrap-login">
+        <div className="login">
+          <img src={DiscordLogo} alt="Discord" />
+          <a href={oauthURL}>Sign in with Discord</a>
+        </div>
       </div>
     </>
   );
